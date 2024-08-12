93f02d8f9573e22a69b51a612fa49397a8c7f0a2
david_llamas_roman <business.llamasromandavid@gmail.com>
2024-08-12 19:57:17 +0200

    Define the port and create the server

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 // imports
 import mongoose from 'mongoose'
+import app from './app.js'
+
+// port
+const port = 3700
 
 // database connection
 mongoose.Promise = global.Promise
@@ -9,5 +13,10 @@ mongoose
   .connect('mongodb://localhost:27017/portfolio')
   .then(() => {
     console.log('Database connection established correctly')
+
+    // server creation
+    app.listen(port, () => {
+      console.log('The server is running correctly in localhost:3700')
+    })
   })
   .catch((e) => console.log(e))
