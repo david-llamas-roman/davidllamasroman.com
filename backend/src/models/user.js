0c6fda473eabc3d969f57a686aa67545b35b256d
david_llamas_roman <business.llamasromandavid@gmail.com>
2024-08-14 19:18:51 +0200

    Create 'User' model

---
+++ backend/src/models/user.js
@@ -0,0 +1,31 @@
+/*
+ * This file is part of davidllamasroman.com.
+ *
+ * davidllamasroman.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * davidllamasroman.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davidllamasroman.com. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ * Copyright (C) 2024 David Llamas Román
+ */
+
+'use strict'
+
+// imports
+import mongoose, { Schema } from 'mongoose'
+
+// user schema
+const user = Schema({
+  fullName: String,
+  email: String,
+  password: String,
+})
+
+export default mongoose.model('User', user)
