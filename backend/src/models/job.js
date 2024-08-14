5b65c532691f360d20d53b545ab65e5decff5589
david_llamas_roman <business.llamasromandavid@gmail.com>
2024-08-14 15:53:17 +0200

    Create 'Job' model

---
+++ backend/src/models/job.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of davidllamasroman.com.
+ *
+ * davidllamasroman.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * davidllamasroman.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davidllamasroman.com. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ * Copyright (C) 2024 David Llamas Román
+ */
+
+'use strict'
+
+// imports
+import model, { Schema } from 'mongoose'
+
+// job schema
+const job = Schema({
+  title: String,
+  employmentType: String,
+  companyName: String,
+  location: String,
+  locationType: String,
+  startDate: Date,
+  endDate: Date,
+  currentlyWorkingHere: Boolean,
+})
+
+export default model('Job', job)
