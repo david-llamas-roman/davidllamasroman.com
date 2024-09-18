2ef32d92efc8d62c9df166d0e6cd591a1a6abc43
david-llamas-roman <business.llamasromandavid@gmail.com>
2024-09-18 23:05:43 +0200

    Create const with API URLs to export and functions to manage them

---
+++ frontend/src/js/apiUrls.js
@@ -0,0 +1,34 @@
+/*
+ * This file is part of davidllamasroman.com.
+ *
+ * davidllamasroman.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * davidllamasroman.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davidllamasroman.com. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ * Copyright (C) 2024 David Llamas Román
+ */
+
+'use strict'
+
+// CONSTANTS
+// dev
+// -> projects
+export const apiUrl = 'http://localhost:3700/api/projects'
+
+// FUNCTIONS
+// data management
+export function getApiUrlData(apiUrl) {
+  return fetch(apiUrl)
+}
+
+export function convertDataToJson(data) {
+  return data.json()
+}
