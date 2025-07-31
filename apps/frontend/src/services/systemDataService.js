ed09f91d17242bebcaf18123618d85586c46fcaf
david-llamas-roman <business.llamasromandavid@gmail.com>
2025-07-31 19:00:09 +0200

    feat: System Data Service
    System data (battery, cpu usage, ram usage...) getters.

---
+++ apps/frontend/src/services/systemDataService.js
@@ -0,0 +1,44 @@
+/*
+ * This file is part of davidllamasroman.com.
+ *
+ * davidllamasroman.com is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License only.
+ *
+ * davidllamasroman.com is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with davidllamasroman.com. If not, see <https://www.gnu.org/licenses/gpl-3.0.en.html>.
+ *
+ * Copyright (C) 2025 David Llamas Román
+ */
+
+'use strict'
+
+import { subscribeSystemData } from '../utils/websocketClient'
+
+const getBatteryPercent = (onUpdate) => {
+  return subscribeSystemData((payload) => {
+    const level = payload?.battery?.percent ?? 0
+    onUpdate?.(Math.round(level))
+  })
+}
+
+const getCpuUsagePercentage = (onUpdate) => {
+  return subscribeSystemData((payload) => {
+    const percentage = payload?.cpu?.usage ?? 0
+    onUpdate?.(Math.round(percentage))
+  })
+}
+
+const getRamUsagePercentage = (onUpdate) => {
+  return subscribeSystemData((payload) => {
+    const percentage = payload?.ram?.usage ?? 0
+    onUpdate?.(Math.round(percentage))
+  })
+}
+
+export { getBatteryPercent, getCpuUsagePercentage, getRamUsagePercentage }
