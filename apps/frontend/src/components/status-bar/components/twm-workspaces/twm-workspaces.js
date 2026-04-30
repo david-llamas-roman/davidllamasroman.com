9160f17c3cda50178acbb1b07f7e50a23ea7735d
David Llamas Román <business.llamasromandavid@gmail.com>
2026-04-30 18:38:57 +0200

    fix: Make the desktop navbar responsive

--- apps/frontend/src/components/status-bar/components/twm-workspaces/twm-workspaces.js
+++ apps/frontend/src/components/status-bar/components/twm-workspaces/twm-workspaces.js
@@ -37,6 +37,16 @@ class TwmWorkspaces extends BaseComponent {
 
     template.innerHTML = `
       ${this.#getStyles()}
+      ${
+        window.innerWidth <= 1435
+          ? `
+          <div class="workspaces-responsive__icons">
+            <img src="/img/google-fonts-icons/menu.svg" alt="Google Fonts Menu Icon" class="workspaces-responsive__icon">
+            <img src="/img/google-fonts-icons/close.svg" alt="Google Fonts Menu Icon" class="workspaces-responsive__icon hide">
+          </div>
+        `
+          : ''
+      }
       <nav class="workspaces">
         <twm-wos-icon></twm-wos-icon>
         <ul class="workspaces__list">
@@ -56,10 +66,97 @@ class TwmWorkspaces extends BaseComponent {
   #getStyles() {
     return `
       <style>
+        .workspaces-responsive__icons {
+          position: relative;
+
+          display: grid;
+          place-items: center;
+
+          height: 100%;
+
+          .workspaces-responsive__icon {
+            width: 30px;
+            aspect-ratio: 1/1;
+
+            visibility: visible;
+            opacity: 1;
+            pointer-events: auto;
+
+            cursor: pointer;
+
+            transition: transform 0.2s, opacity 0.2s;
+
+            &:hover {
+              transform: scale(1.1);
+            }
+
+            &.hide {
+              visibility: hidden;
+              opacity: 0;
+              pointer-events: none;
+            }
+
+            &:nth-child(1) {
+              position: absolute;
+            }
+
+            &:nth-child(2) {
+              filter: brightness(0) invert(1);
+            }
+          }
+        }
+
         .workspaces {
           display: flex;
           align-items: center;
           gap: 1rem;
+
+          transition: opacity 0.2s;
+
+          @media (width <= 1435px) {
+            position: absolute;
+            top: 0.6rem;
+            left: 4.25rem;
+            z-index: 1;
+
+            visibility: hidden;
+            opacity: 0;
+            pointer-events: none;
+
+            flex-direction: column;
+
+            padding: 1rem 1.5rem 1.6rem 1.5rem;
+
+            background-image: linear-gradient(var(--black, #000), var(--dark-blue, #05020e));
+
+            border: 1px solid var(--white, #fff);
+            border-radius: 4px;
+
+            box-shadow: 0 0 1rem rgba(0, 0, 0, 0.6);
+
+            .workspaces__list {
+              flex-direction: column;
+
+              .list__element {
+                width: 100%;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                .element__link {
+                  flex-grow: 1;
+                  flex-shrink: 0;
+                }
+              }
+            }
+          }
+
+          &.active {
+            visibility: visible;
+            opacity: 1;
+            pointer-events: auto;
+          }
 
           .workspaces__list {
             display: flex;
@@ -112,6 +209,29 @@ class TwmWorkspaces extends BaseComponent {
 
     this.shadowRoot.appendChild(this.#getTemplate().content.cloneNode(true))
 
+    this.shadowRoot
+      .querySelector('.workspaces-responsive__icon:nth-child(1)')
+      .addEventListener('click', () => {
+        const workspaces = this.shadowRoot.querySelector('.workspaces')
+        workspaces.classList.add('active')
+
+        const menuIcon = this.shadowRoot.querySelector(
+          '.workspaces-responsive__icon:nth-child(1)',
+        )
+        menuIcon.classList.add('hide')
+
+        const closeIcon = this.shadowRoot.querySelector(
+          '.workspaces-responsive__icon:nth-child(2)',
+        )
+        closeIcon.classList.remove('hide')
+
+        closeIcon.addEventListener('click', () => {
+          menuIcon.classList.remove('hide')
+          workspaces.classList.remove('active')
+          closeIcon.classList.add('hide')
+        })
+      })
+
     this.shadowRoot.querySelectorAll('.element__link').forEach((anchor) =>
       anchor.addEventListener('click', (event) => {
         event.preventDefault()
@@ -133,6 +253,8 @@ class TwmWorkspaces extends BaseComponent {
   connectedCallback() {
     this.render()
 
+    window.addEventListener('resize', this.#handleResize)
+
     window.addEventListener('workspace:switch', (event) => {
       const id = event?.detail?.id
       if (id) this.#setActiveWorkspace(id)
@@ -142,6 +264,10 @@ class TwmWorkspaces extends BaseComponent {
       const workspaceId = findWorkspaceIdFromPath()
       if (workspaceId) this.#setActiveWorkspace(workspaceId)
     })
+  }
+
+  #handleResize = () => {
+    this.render()
   }
 
   #setActiveWorkspace(id) {
